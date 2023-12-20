440eac8b981027f80d4cb7e98bbb675cde939046
dimakozlovich <kozlovichdima21@gmail.com>
2023-12-20 21:34:11 +0300

    add test

---
+++ WebDiaryTests/Helpers/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDiaryVersion1.BL.Auth;
+using WebDiaryVersion1.DLL.Auth_DLL;
+
+namespace WebDiaryTests.Helpers
+{
+    public class BaseTest
+    {
+        protected IAuthDAL authDall = new AuthDAL();
+        protected IEncrypt encrypt = new Encrypt();
+        protected IHttpContextAccessor httpContextAccessor = new HttpContextAccessor();
+        protected IDbSessionDAL dbSessionDall = new DbSessionDAL();
+        protected IDbSession dbSession;
+        protected IAuthBL authBL;
+
+        public BaseTest()
+        {
+            dbSession = new DbSession(dbSessionDall,httpContextAccessor);
+            authBL = new AuthBL(authDall, encrypt, httpContextAccessor,dbSession);
+        }
+
+
+    }
+}
---
+++ WebDiaryTests/RegisterTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDiaryTests.Helpers;
+
+namespace WebDiaryTests
+{
+    public class RegisterTest : BaseTest
+    {
+        [Fact]
+        public async Task BaseRegistrationTest()
+        {
+            string email = Guid.NewGuid().ToString() + "@test.com";
+
+            // validate: should not be in the DB
+            var emailValidationResult = await authBL.ValidateEmail(email);
+            Assert.Null(emailValidationResult);
+
+            int user_id = await authBL.CreateUser(
+                new WebDiaryVersion1.DLL.Models_DLL.UserModel()
+                {
+                    Email = email,
+                    Password = "testpassword"
+                }
+                ) ;
+
+            Assert.NotEqual(expected: 0, user_id);
+
+            emailValidationResult = await authBL.ValidateEmail(email);
+            Assert.NotNull(emailValidationResult);
+        }
+    }
+}
